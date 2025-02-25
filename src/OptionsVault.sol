5d745d4baaf3f52b8b7e94bab916c8465c67ff4b
rishabh0x00 <rsharma0x00@gmail.com>
2025-02-25 23:21:53 +0000

    add: functions to buy, exercise options

--- src/OptionsVault.sol
+++ src/OptionsVault.sol
@@ -70,10 +70,11 @@ contract OptionsVault is IERC721Receiver {
     ) internal {
         (uint256 asset1Amt, uint256 asset2Amt, address asset1) = _withdrawAssets(tokenId);
 
+        (string memory optionName, string memory optionSymbol) = _generateOptionMetadata(asset1, strikePrice, isCall);
         address option = Clones.clone(optionImplementation);
         OptionERC20(option).initialize(
-            "Option", // TODO 
-            "OP",
+            optionName,
+            optionSymbol,
             strikePrice,
             expiry,
             isCall,
@@ -92,6 +93,51 @@ contract OptionsVault is IERC721Receiver {
         );
         options[tokenId] = data;
         optionByAddress[option] = data;
+    }
+
+    // Buy options from creator
+    function buyOption(
+        address optionAddress,
+        uint256 amount,
+        uint256 premium
+    ) external {
+        OptionData memory data = optionByAddress[optionAddress];
+        require(data.creator != address(0), "Invalid option");
+
+        OptionERC20 option = OptionERC20(optionAddress);
+        IERC20(data.asset2).transferFrom(msg.sender, data.creator, premium);
+        option.adminTransfer(data.creator, msg.sender, amount);
+    }
+
+    // Exercise options
+    function exerciseOption(address optionAddress, uint256 amount) external {
+        OptionERC20 option = OptionERC20(optionAddress);
+        require(block.timestamp >= option.expiry() - 1 hours, "Window closed");
+
+        (bool profitable, uint256 profit) = option.calculateProfit(
+            msg.sender,
+            amount
+        );
+        if (profitable) {
+            _transferAssets(optionAddress, profit, option.terms().optionType);
+        }
+        option.burn(msg.sender, amount);
+    }
+
+    // Asset transfer logic
+    function _transferAssets(
+        address optionAddress,
+        uint256 profit,
+        OptionERC20.OptionType oType
+    ) internal {
+        OptionData storage data = optionByAddress[optionAddress];
+        if (oType == OptionERC20.OptionType.Call) {
+            IERC20(data.asset2).transfer(msg.sender, profit);
+            data.asset2Amount -= profit;
+        } else {
+            IERC20(data.asset1).transfer(msg.sender, profit);
+            data.asset1Amount -= profit;
+        }
     }
 
     // Helper to get pool address from NFT
@@ -155,6 +201,47 @@ contract OptionsVault is IERC721Receiver {
             amount0 + collected0,
             amount1 + collected1,
             token0
+        );
+    }
+
+    function _formatStrikePrice(uint256 price, uint8 decimals) internal pure returns (string memory) {
+        uint256 formattedPrice = price / 10 ** decimals;
+        return _uintToString(formattedPrice);
+    }
+
+    function _uintToString(uint256 value) internal pure returns (string memory) {
+        if (value == 0) return "0";
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+        return string(buffer);
+    }
+
+    function _generateOptionMetadata(
+        address asset1,
+        uint256 strikePrice,
+        bool isCall
+    ) internal view returns (string memory, string memory) {
+        string memory assetSymbol = ERC20(asset1).symbol();
+        uint8 decimals = ERC20(asset1).decimals();
+        string memory strikePrice = _formatStrikePrice(strikePrice, decimals);
+
+        // Generate name and symbol
+        string memory typeStr = isCall == true ? "Call" : "Put";
+        string memory typeSymbol = isCall == true ? "C" : "P";
+        
+        return (
+            string(abi.encodePacked(assetSymbol, " ", typeStr, " ", strikePrice)),
+            string(abi.encodePacked(assetSymbol, "-", typeSymbol, "-", strikePrice))
         );
     }
 }
