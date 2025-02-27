5b008c5315745b1a660e8aed9ed25fd376f3808a
Rishabh Sharma <rsharma0x00@gmail.com>
2025-02-27 14:20:35 +0530

    Wallet context for storing necessary wallet info

---
+++ frontend/src/context/WalletContext.js
@@ -0,0 +1,61 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+import { ethers } from "ethers";
+
+const WalletContext = createContext();
+
+export const WalletProvider = ({ children }) => {
+  const [isConnected, setIsConnected] = useState(false);
+  const [account, setAccount] = useState(null);
+  const [loading] = useState(false);
+  const [provider, setProvider] = useState(null);
+  const [signer, setSigner] = useState(null);
+
+  const connectWallet = async () => {  
+    if (window.ethereum) {
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setIsConnected(true);
+        setAccount(accounts[0]);
+
+        const browserProvider = new ethers.BrowserProvider(window.ethereum);
+        const userSigner = await browserProvider.getSigner();
+
+        setProvider(browserProvider);
+        setSigner(userSigner);
+      } catch (error) {
+        console.error("Error connecting to MetaMask", error);
+      }
+    } else {
+      alert("MetaMask is not installed!");
+    }
+  };
+
+  const disconnectWallet = () => {
+    setIsConnected(false);
+    setAccount(null);
+    setProvider(null);
+    setSigner(null);
+  };
+
+  return (
+    <WalletContext.Provider
+      value={{
+        isConnected,
+        account,
+        connectWallet, 
+        disconnectWallet,
+        loading,
+        provider,
+        signer,
+      }}
+    >
+      {children}
+    </WalletContext.Provider>
+  );
+};
+
+export const useWallet = () => useContext(WalletContext);
