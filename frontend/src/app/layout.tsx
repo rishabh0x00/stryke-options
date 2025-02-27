30c1c3bb806695ab15ea908cc0e69b1e01d801e8
Rishabh Sharma <rsharma0x00@gmail.com>
2025-02-27 15:11:19 +0530

    App layout

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,16 @@
+import "./globals.css";
+import { WalletProvider } from "@/context/WalletContext";
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body className="relative h-full w-full bg-white">
+        <WalletProvider>
+          <div className="glass-container w-full h-full">{children}</div>
+        </WalletProvider>
+      </body>
+    </html>
+  );
+}
